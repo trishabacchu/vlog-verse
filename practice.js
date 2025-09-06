9b6ef0cbb43cd187b1d52dc1363595b415d8555f
trishabacchu <trishabacchu84@gmail.com>
2025-09-06 12:37:47 +0530

    js

--- practice.js
+++ practice.js
@@ -1,1 +1,29 @@
 console.log("hello world,trisha")
+let name = "trisha";
+console.log(name)
+let num=18;
+console.log(num)
+let islogged=false;
+console.log(islogged)
+
+let x;
+console.log(x)
+let y=null;
+console.log(y)
+console.log(typeof name)
+console.log(typeof num)
+console.log(typeof islogged)
+console.log(x)
+console.log(typeof y)
+
+let arr=[1,2,3,4,5]
+console.log(arr[0])
+console.log(typeof arr)
+
+let person = {
+    name:"trisha",
+    age:20
+}
+console.log(person.name)
+console.log(person.age)
+console.log(typeof person)
